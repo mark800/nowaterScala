5c975c304a9ab82dc9617080d18d636f40485ad8
yky <yky800@126.com>
2016-02-02 19:23:24 +0800

    update

--- src/main/scala/com/ky/ExpandLyric.scala
+++ src/main/scala/com/ky/ExpandLyric.scala
@@ -2,22 +2,20 @@ package com.ky
 
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
-//所有的return 可以完全省略掉
+
 object ExpandLyric {
 
   def main(args: Array[String]) {
-    val output = expand(getLrcAsSeq())
-    output.foreach { x => println(x) }
+    //val output = expand(getLrcAsSeq())
+    getLrcAsSeq().foreach { x => println(x) }
   }
 
   val tmarker = """(\[\d\d:\d\d\.\d\d\])""".r
 
   def getLrcAsSeq(): Seq[String] = {
     val source = Source.fromURL(getClass.getResource("/two_tigers.txt"), "utf-8")
-    //val res: Array[String] = sampleInput.split("\n")
-    val lines = try source.getLines mkString "\n" finally source.close()//这个地方为什么 mkString, 下面又split了？ 直接 source.getLines？
-    //如果用try finally 的话 考虑下Try
-    return lines.split("\n")
+    val lines = try source.getLines mkString "\n" finally source.close()
+    lines.split("\n")
   }
 
   def containTimeMarker(aline: String): Boolean = aline match {
@@ -31,7 +29,7 @@ object ExpandLyric {
     all map (_ + lyric)
   }
 
-  def expand(input: Seq[String]): Seq[String] = {//可用 fold/reduce  + Tuple2 来实现
+  def expand(input: Seq[String]): Seq[String] = {
     val all = new ListBuffer[String]
     val lyrics = new ListBuffer[String]
 
